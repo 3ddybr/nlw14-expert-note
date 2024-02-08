0b57cc4ec0ed34b595d5239fc76adc58ecc744fc
3ddybr <3ddy.br@gmail.com>
2024-02-08 16:14:23 -0300

    aula 3

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports={
+  "rules": {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"]
+},
+
+  "root": true,
+  "extends": [
+    "@rocketseat/eslint-config/react"
+  ]
+}
